111b6c83f52fb33a80dfbf2e92f18a1f78578e7f
Ignaphim <test@localhost>
2022-06-07 13:39:54 +0100

    Estrutura v3 07/06/2022 - 13:40

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,6 +4,9 @@ export default function Footer() {
     return (
         <footer>
             <h3>Footer</h3>
+            <li><a href="/">Home</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="/aboutme">About me</a></li>
         </footer>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-        Home
+    <div role="content">
+      Home
+
+      <div>
+        <h3>Fábio Lascadinha</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris enim arcu, mattis a metus ac, posuere interdum nulla. Ut erat orci, euismod vel bibendum id, blandit eu nisl. Vestibulum rhoncus semper nunc id commodo. In et lobortis dui, in dignissim lacus. Nullam condimentum at felis sit amet aliquet.</p>
+      </div>
     </div>
   )
 }
