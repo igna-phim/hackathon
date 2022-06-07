3abd0cac904af6974161019c76b6a61a0d380b84
Ignaphim <test@localhost>
2022-06-07 13:06:30 +0100

    Estrutura v2 07/06/2022 - 13:06

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,11 @@
-import Header from "./Header";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 
 export default function Layout({ children }) {
     return (
         <>
-            <Header />
+            <Navbar />
             <main>{children}</main>
             <Footer />
         </>
